571dc0938962bc62854c383f8af4c23f666e045c
Martin Evans <martindevans@gmail.com>
2011-05-08 23:19:33 +0100

    Built and tested SLR parser
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- Hermes/Parsers/SLR1.cs
+++ Hermes/Parsers/SLR1.cs
@@ -1,17 +1,28 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using Hermes.Bnf;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hermes.Bnf;
+using Hermes.Tokenising;
 
-//namespace Hermes.Parsers
-//{
-//    public class SLR1
-//        :LRParserBase
-//    {
-//        public SLR1(Grammar grammar)
-//            :base(grammar)
-//        {
-//        }
-//    }
-//}
+namespace Hermes.Parsers
+{
+    public class SLR1
+        : LR0
+    {
+        public SLR1(Grammar grammar)
+            : base(grammar)
+        {
+        }
+
+        protected override ParserAction Action(ParseState state, Token token, out Production production, out ParseState nextState)
+        {
+            var lr0 = base.Action(state, token, out production, out nextState);
+
+            if (token != null && lr0 == ParserAction.Reduce && !Grammar.GetFollowSet(production.Head).Contains(token.Terminal))
+                return ParserAction.Error;
+
+            return lr0;
+        }
+    }
+}
--- HermesTests/SLR1Test.cs
+++ HermesTests/SLR1Test.cs
@@ -5,43 +5,17 @@ using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Hermes.Bnf;
 using Hermes.Parsers;
-using HermesTests.Grammars;
 
 namespace HermesTests
 {
     [TestClass]
-    public class LR0Test
+    public class SLR1Test
     {
-        public static Grammar ConstructTestGrammar(out NonTerminal S, out NonTerminal T, out NonTerminal F)
-        {
-            S = new NonTerminal("S");
-            T = new NonTerminal("T");
-            F = new NonTerminal("F");
-
-            S.Rules = T;
-            T.Rules = T + "*" + F | F;
-            F.Rules = "(" + T + ")" | new Terminal("ID", "([A-Z]|[a-z])+");
-
-            return new Grammar(S, new Terminal(" ", isIgnored:true));
-        }
-
-        [TestMethod]
-        public void ConstructAutomaton()
-        {
-            NonTerminal S, T, F;
-            Grammar g = ConstructTestGrammar(out S, out T, out F);
-
-            Automaton a = LR0.CreateAutomaton(g);
-
-            Assert.AreEqual(4, a.Terminals.Count());
-            Assert.AreEqual(9, a.States.Count());
-        }
-
         [TestMethod]
         public void ParseValidString()
         {
             NonTerminal S, T, F;
-            LR0 parser = new LR0(ConstructTestGrammar(out S, out T, out F));
+            LRParserBase parser = new SLR1(LR0Test.ConstructTestGrammar(out S, out T, out F));
 
             ParseTree tree = parser.Parse("((x) * t) * foo");
 
@@ -62,7 +36,7 @@ namespace HermesTests
         public void ParseInvalidEarlyTerminatingString()
         {
             NonTerminal S, T, F;
-            LR0 parser = new LR0(ConstructTestGrammar(out S, out T, out F));
+            LR0 parser = new SLR1(LR0Test.ConstructTestGrammar(out S, out T, out F));
 
             var tree = parser.Parse("((x)*t)*");
         }
@@ -72,7 +46,7 @@ namespace HermesTests
         public void ParseInvalidUnexpectedCharacterString()
         {
             NonTerminal S, T, F;
-            LR0 parser = new LR0(ConstructTestGrammar(out S, out T, out F));
+            LR0 parser = new SLR1(LR0Test.ConstructTestGrammar(out S, out T, out F));
 
             var tree = parser.Parse("((x)*t)**");
         }
